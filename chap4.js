05c8d5d809a51ec670748678aa35abfe5811ffdf
Iris Yuan <builtbyiris@gmail.com>
2016-12-01 23:53:27 -0600

    partially done chap 4

---
+++ chap4.js
@@ -0,0 +1,66 @@
+// Chapter 4 solutions
+
+// Problem Sum of a range and range step
+function range(start, end) {
+	var arr = [];
+	for (var i=start; i <= end; i++) {
+		arr.push(i);
+	}
+	return arr;
+}
+//console.log(range(1,10));
+function sum(array) {
+	var sum = 0;
+	for (var i=0; i < array.length;i++){
+		sum += array[i];
+	}
+	return sum;
+}
+//console.log(sum(range(1,1)));
+/* Modified range takes third arg to accept "step" of increment
+function range(start, end, step) {
+	if (step == null) step = 1;
+	var arr = [];
+	if (step > 0 && start < end) {
+		for (var i=start; i <= end; i += step) {
+			arr.push(i);
+		}
+	} else if (step < 0 && start > end) {
+		for (var i=start; i >=end; i+=step) {
+			arr.push(i);
+		}
+	} else {
+		console.log("Starting value must be less than end if step is positive");
+	}
+	return arr;
+}
+console.log(range(1,10,-2));
+*/
+
+
+// Problem 2 Reversing an Array
+function reverseArray(oldArray) {
+	var newArr = [];
+	for (var i=oldArray.length-1; i >= 0; i--){
+		newArr.push(oldArray[i]);
+	}
+	return newArr;
+}
+//console.log(reverseArray(["A", "B", "C"]));
+
+function reverseArrayInPlace(oldArray){
+	var iterations = Math.floor(oldArray.length/2);
+	for (var i=0; i< iterations;i++) {
+		oldArray[i] = oldArray[iterations-i];
+		console.log(i + ": " + oldArray);
+		
+	}
+	return oldArray;
+}
+console.log(reverseArrayInPlace(["A", "B", "C","D"]));
+
+// Problem 3 A List
+
+// Problem 4 Deep Comparison
+
+
